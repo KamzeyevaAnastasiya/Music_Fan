8a6b9d71019382bf2654e63a87ea0f46a87b2c65
KamzeyevaAnastasiya <nastya.ermakovich.17@mail.ru>
2026-04-12 16:23:49 +0300

    Add hook for global loading

--- src/app/ui/App.tsx
+++ src/app/ui/App.tsx
@@ -1,12 +1,16 @@
-import { Header } from '@/common/components'
+import { Header, LinearProgress } from '@/common/components'
+import { useGlobalLoading } from '@/common/hooks/useGlobalLoading'
 import { Routing } from '@/common/routing/Routing'
 import { ToastContainer } from 'react-toastify'
 import s from './App.module.css'
 
 export const App = () => {
+  const isGlobalLoading = useGlobalLoading()
+
   return (
     <>
       <Header />
+      {isGlobalLoading && <LinearProgress />}
       <div className={s.layout}>
         <Routing />
       </div>
