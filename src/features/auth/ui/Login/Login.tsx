4cc9e107a555883cbfc4ee5f4a5419692cbfd04d
KamzeyevaAnastasiya <nastya.ermakovich.17@mail.ru>
2026-06-19 18:45:55 +0300

    Create loginHandler to oauth

--- src/features/auth/ui/Login/Login.tsx
+++ src/features/auth/ui/Login/Login.tsx
@@ -1,10 +1,28 @@
+import { Path } from '@/common/routing/Path'
 import { useLoginMutation } from '@/features/auth/api/authApi'
 
 export const Login = () => {
   const [login] = useLoginMutation()
 
   const loginHandler = () => {
-    login({ code: '', redirectUri: '', rememberMe: false })
+    const redirectUri = import.meta.env.VITE_DOMAIN_ADDRESS + Path.OAuthRedirect
+
+    const url = `${import.meta.env.VITE_BASE_URL}/auth/oauth-redirect?callbackUrl=${redirectUri}`
+
+    window.open(url, 'oauthPopup', 'width=600,height=600')
+
+    const receiveMessage = (event: MessageEvent) => {
+      if (event.origin !== import.meta.env.VITE_DOMAIN_ADDRESS) return
+
+      const { code } = event.data
+      if (!code) return
+
+      window.removeEventListener('message', receiveMessage)
+
+      login({ code, redirectUri, rememberMe: false })
+    }
+
+    window.addEventListener('message', receiveMessage)
   }
 
   return (
