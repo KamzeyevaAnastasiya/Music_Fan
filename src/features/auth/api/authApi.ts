e30057ee71b3269635456598924c70f94594a088
KamzeyevaAnastasiya <nastya.ermakovich.17@mail.ru>
2026-06-23 17:29:15 +0300

    Add tokens to login and create logout endpoint

--- src/features/auth/api/authApi.ts
+++ src/features/auth/api/authApi.ts
@@ -1,10 +1,12 @@
 import { baseApi } from '@/app/api/baseApi'
+import { AUTH_KEYS } from '@/common/constants'
 import type { LoginArgs, LoginResponse, MeResponse } from '@/features/auth/api/authApi.types'
 
 export const authApi = baseApi.injectEndpoints({
   endpoints: (build) => ({
     getMe: build.query<MeResponse, void>({
       query: () => `auth/me`,
+      providesTags: ['Auth'],
     }),
     login: build.mutation<LoginResponse, LoginArgs>({
       query: (payload) => ({
@@ -12,8 +14,30 @@ export const authApi = baseApi.injectEndpoints({
         method: 'post',
         body: { ...payload, accessTokenTTL: '3m' },
       }),
+      onQueryStarted: async (_args, { dispatch, queryFulfilled }) => {
+        const { data } = await queryFulfilled
+        localStorage.setItem(AUTH_KEYS.accessToken, data.accessToken)
+        localStorage.setItem(AUTH_KEYS.refreshToken, data.refreshToken)
+        dispatch(authApi.util.invalidateTags(['Auth']))
+      },
+    }),
+    logout: build.mutation<void, void>({
+      query: () => {
+        const refreshToken = localStorage.getItem(AUTH_KEYS.refreshToken)
+        return {
+          url: `auth/logout`,
+          method: 'post',
+          body: { refreshToken },
+        }
+      },
+      onQueryStarted: async (_args, { dispatch, queryFulfilled }) => {
+        await queryFulfilled
+        localStorage.removeItem(AUTH_KEYS.accessToken)
+        localStorage.removeItem(AUTH_KEYS.refreshToken)
+        dispatch(baseApi.util.resetApiState())
+      },
     }),
   }),
 })
 
-export const { useGetMeQuery, useLoginMutation } = authApi
+export const { useGetMeQuery, useLoginMutation, useLogoutMutation } = authApi
