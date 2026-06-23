e7feb878c26ec2a1fec906b032f87ca5e33f43b2
KamzeyevaAnastasiya <nastya.ermakovich.17@mail.ru>
2026-06-23 22:34:28 +0300

    Add tracks to  my playlist

--- src/features/auth/ui/ProfilePage/ProfilePage.tsx
+++ src/features/auth/ui/ProfilePage/ProfilePage.tsx
@@ -1,11 +1,18 @@
 import { useGetMeQuery } from '@/features/auth/api/authApi'
+import { useFetchPlaylistsQuery } from '@/features/playlists/api/playlistsApi'
+import { PlaylistsList } from '@/features/playlists/ui/PlaylistsPage/PlaylistsList/PlaylistsList'
 
 export const ProfilePage = () => {
-  const { data } = useGetMeQuery()
+  const { data: meResponse } = useGetMeQuery()
+
+  const { data: PlaylistsResponse, isLoading } = useFetchPlaylistsQuery({
+    userId: meResponse?.userId,
+  })
 
   return (
     <div>
-      <h1>{data?.login} page</h1>
+      <h1>{meResponse?.login} page</h1>
+      <PlaylistsList playlists={PlaylistsResponse?.data || []} isPlaylistsLoading={isLoading} />
     </div>
   )
 }
