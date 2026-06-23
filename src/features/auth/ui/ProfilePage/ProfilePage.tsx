d4288b747f1dfab4c57340f896bb4e8e88b9f71f
KamzeyevaAnastasiya <nastya.ermakovich.17@mail.ru>
2026-06-23 22:55:53 +0300

    Add redirect after logout and sceleton for loading

--- src/features/auth/ui/ProfilePage/ProfilePage.tsx
+++ src/features/auth/ui/ProfilePage/ProfilePage.tsx
@@ -1,22 +1,31 @@
+import { Path } from '@/common/routing/Path'
 import { useGetMeQuery } from '@/features/auth/api/authApi'
 import { useFetchPlaylistsQuery } from '@/features/playlists/api/playlistsApi'
 import { CreatePlaylistForm } from '@/features/playlists/ui/PlaylistsPage/CreatePlaylistForm/CreatePlaylistForm'
 import { PlaylistsList } from '@/features/playlists/ui/PlaylistsPage/PlaylistsList/PlaylistsList'
+import { Navigate } from 'react-router'
 import s from './ProfilePage.module.css'
 
 export const ProfilePage = () => {
-  const { data: meResponse } = useGetMeQuery()
+  const { data: meResponse, isLoading: isMeLoading } = useGetMeQuery()
 
-  const { data: PlaylistsResponse, isLoading } = useFetchPlaylistsQuery({
-    userId: meResponse?.userId,
-  })
+  const { data: PlaylistsResponse, isLoading } = useFetchPlaylistsQuery(
+    {
+      userId: meResponse?.userId,
+    },
+    { skip: !meResponse?.userId },
+  )
+
+  if (isLoading || isMeLoading) return <h1>Skeleton loader...</h1>
+
+  if (!isMeLoading && !meResponse) return <Navigate to={Path.Playlists} />
 
   return (
     <div>
       <h1>{meResponse?.login} page</h1>
       <div className={s.container}>
         <CreatePlaylistForm />
-        <PlaylistsList playlists={PlaylistsResponse?.data || []} isPlaylistsLoading={isLoading} />
+        <PlaylistsList playlists={PlaylistsResponse?.data || []} isPlaylistsLoading={isLoading || isMeLoading} />
       </div>
     </div>
   )
