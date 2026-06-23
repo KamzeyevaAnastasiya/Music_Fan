be9231a403a2efab69312e4e403981d4684d1007
KamzeyevaAnastasiya <nastya.ermakovich.17@mail.ru>
2026-06-23 17:29:54 +0300

    Add logout to header and styles

--- src/common/components/Header/Header.tsx
+++ src/common/components/Header/Header.tsx
@@ -1,5 +1,5 @@
 import { Path } from '@/common/routing/Path'
-import { useGetMeQuery } from '@/features/auth/api/authApi'
+import { useGetMeQuery, useLogoutMutation } from '@/features/auth/api/authApi'
 import { Login } from '@/features/auth/ui/Login/Login'
 import { NavLink } from 'react-router'
 import s from './Header.module.css'
@@ -13,6 +13,11 @@ const navItems = [
 
 export const Header = () => {
   const { data } = useGetMeQuery()
+  const [logout] = useLogoutMutation()
+
+  const logoutHandler = () => {
+    logout()
+  }
 
   return (
     <header className={s.container}>
@@ -27,7 +32,12 @@ export const Header = () => {
           ))}
         </ul>
       </nav>
-      {data && data.login}
+      {data && (
+        <div className={s.loginContainer}>
+          <p>{data.login}</p>
+          <button onClick={logoutHandler}>logout</button>
+        </div>
+      )}
       {!data && <Login />}
     </header>
   )
